df94b6a73aaf084a46587ccdd82f597a8697fba3
antfuentes87 <antfuentes87@gmail.com>
2018-06-20 11:05:09 -0500

    lesson on classes

---
+++ classes/main.py
@@ -0,0 +1,36 @@
+class Printer:
+	def __init__(self, name):
+		self.name = name
+
+	def print_add(self, num):
+		tmpl = 'My answer is {} and my name is {}'
+		print (tmpl.format(num, self.name))
+		return
+
+class Calc:
+	def __init__(self):
+		self.printer = Printer('Josh')
+
+	def add_times(self, x, y):
+		num = self.add(x, y) + self.times(x, y)
+		self.printer.print_add(num)
+		return 
+
+	def add(self, x, y):
+		return x + y
+
+	def times(self, x, y):
+		return x * y
+
+	def divide(self, x, y):
+		return x / y
+
+	def minus(self, x, y):
+		return x - y
+
+if __name__ == '__main__':
+	calc = Calc()
+	printer = Printer('Josh')
+
+	calc.add_times(1, 2)
+	printer.print_add(12)
---
+++ classes/using_name.py
@@ -0,0 +1,5 @@
+import main
+
+if __name__ == '__main__':
+	my_answer = main.add(1, 2)
+	print (my_answer)
